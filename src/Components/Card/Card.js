bbe36e9c65094c77765154a3532b6198c271261b
ajoy biswas <ajoypust@gmail.comm>
2022-04-24 19:31:51 +0600

    framer motion add

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Benefit from '../Benefit/Benefit';
-import './Card.css'
+import './Card.css';
+import { motion } from "framer-motion"
 const Card = (props) => {
     const{name,price,benifets}=props.cardDetail;
     
     return (
-        <div className='bg-green-400 p-8 rounded-lg hover-effect'>
+        <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 2 }}
+        className='bg-green-400 p-8 rounded-lg hover-effect'>
             <h1 className='text-white text-4xl mb-5 bg-green-300 p-5 font-bold uppercase rounded-lg' >{name}</h1>
             <p className='text-4xl font-bold'> 
                 <small >${price}<span className='text-gray-600 text-2xl'>/mon</span></small>
@@ -16,7 +20,7 @@ const Card = (props) => {
                 benifit={benifit}
                 ></Benefit>)
             }
-        </div>
+        </motion.div>
     );
 };
 
